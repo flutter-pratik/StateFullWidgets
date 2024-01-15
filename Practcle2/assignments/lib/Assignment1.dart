4dbb15177bbb3093da6b4538c9e45e098030bf45
flutter-pratik <flutterpratik@gmail.com>
2024-01-15 18:53:10 +0530

    table of 2

---
+++ Practcle2/assignments/lib/Assignment1.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class Assignment1 extends StatefulWidget {
+  const Assignment1({super.key});
+
+  @override
+  State<Assignment1> createState() => _Assignment1();
+}
+
+class _Assignment1 extends State<Assignment1> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter += 2;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("table of number"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Table of 2',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
